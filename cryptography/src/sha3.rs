6138d0be5e2c6e88fde11afb05d26cbdb5f27a5c
justice32 <vlad3079@gmail.com>
2024-12-15 12:53:29 -0500

    sha3

--- cryptography/src/sha3.rs
+++ cryptography/src/sha3.rs
@@ -6,10 +6,16 @@ pub fn exec() {
 
     hasher.update(b"abc");
 
+    //println!("{:?}", &hasher.clone().decompose());
+
     let result = hasher.finalize();
 
     println!("{:?}",result);
 
+
+    let arr = hex!("3a985da74fe225b2045c172d6bd390bd855f086e3e9d525b46bfe24511431532");
+
+    println!("{:?}", arr);
 
     assert_eq!(result[..], hex!("3a985da74fe225b2045c172d6bd390bd855f086e3e9d525b46bfe24511431532")[..])
 }
